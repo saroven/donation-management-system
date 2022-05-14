4e3d1e9d1a664bb68a36766c7b18a1a9321db2c3
Mohammad Shah Alam <ashah3562@gmail.com>
2022-05-15 02:09:09 +0600

    show counts on dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Donations;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -13,6 +15,12 @@ class DashboardController extends Controller
 
     public function showDashboard()
     {
-        return view('admin.dashboard');
+
+        return view('admin.dashboard', [
+            'allDonation' => Donations::all(),
+            'successfulDonation' => Donations::where('status', 1)->get(),
+            'pendingDonation' => Donations::where('status', 0)->get(),
+            'users' => User::all()
+        ]);
     }
 }
