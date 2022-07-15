094ada7b307eac0e89606855b916fa8c2107749d
saroven <ashah3562@gmail.com>
2022-07-15 23:08:40 +0600

    share siteData to all views and displayed site title on dashboard footer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //share the settings(site info) to all views
+         View::share('siteData', Setting::first());
+
     }
 }
