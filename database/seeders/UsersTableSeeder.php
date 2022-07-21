6aca01e09258cf17c58f127e37511fd84ef01b44
saroven <ashah3562@gmail.com>
2022-07-21 11:56:04 +0600

    add more userinfo in the userstableseeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,19 +19,64 @@ class UsersTableSeeder extends Seeder
     {
 
 
-        $user = User::where('email', 'saroven@yahoo.com')->first();
+        $user = User::all()->count();
 
-        if ($user) {
+        if ($user != 0) {
             $this->command->info('Users table already seeded');
             return;
         }else{
             $this->command->info('Seeding Users table');
+
+            // Create admin user
             User::create([
-                'name' => 'Admin',
+                'name' => 'Mohammad Shah Alam',
                 'email' => 'saroven@yahoo.com',
                 'mobile' => '01800000000',
                 'gender' => 'male',
                 'address' => 'Dhaka',
+                'user_type' => 1,
+                'password' => Hash::make('123456'),
+            ]);
+            //create admin user
+            User::create([
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'mobile' => '01800000000',
+                'gender' => 'male',
+                'address' => 'dhaka',
+                'user_type' => 1,
+                'password' => Hash::make('123456'),
+            ]);
+
+            User::create([
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'mobile' => '01800000000',
+                'gender' => 'male',
+                'address' => 'dhaka',
+                'user_type' => 1,
+                'password' => Hash::make('123456'),
+            ]);
+
+            //create volunteer user
+            User::create([
+                'name' => 'volunteer',
+                'email' => 'volunteer@gmail.com',
+                'mobile' => '01800000000',
+                'gender' => 'male',
+                'address' => 'dhaka',
+                'user_type' => 2,
+                'password' => Hash::make('123456'),
+            ]);
+
+            //create donor user
+            User::create([
+                'name' => 'donor',
+                'email' => 'donor@gmail.com',
+                'mobile' => '01800000000',
+                'gender' => 'male',
+                'address' => 'dhaka',
+                'user_type' => 3,
                 'password' => Hash::make('123456'),
             ]);
         }
